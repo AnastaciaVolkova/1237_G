e129676877db4c06aaba36075e4b69f96e4eed0d
Anastacia Volkova <polarbolide@gmail.com>
2020-03-01 22:28:40 +0100

    Algorithm should have check, if  neighbours were already balanced.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,8 @@ int main() {
     CircleListAdvance<int> cir = {3, 2, 6, 1, 10, 3, 2};
     CircleListAdvance<int>::PQueue mm;
     cir.ComputeSums(mm);
-    set<CircleListAdvance<int>::iterator> me_inv; // Meetings which sums were invalidated.
+    set<CircleListAdvance<int>::MeetingSum> me_inv; // Meetings which sums were invalidated.
+    set<CircleListAdvance<int>::iterator> nei_inv; // Neighbours who already has taken part in meetings.
 
     CircleListAdvance<int>::MeetingSum a;
 
@@ -16,11 +17,14 @@ int main() {
         a = mm.top();
         mm.pop();
 
-        if (me_inv.find(a.nei) == me_inv.end()){
+        if (me_inv.find({ a.nei, a.sum }) == me_inv.end()) {
             cir.Balance(a.nei);
             CircleListAdvance<int>::iterator it = a.nei;
+            nei_inv.insert(it);
             ++it;
             for(int i = 1; i < cir.size(); i++){
+                me_inv.insert(a);
+                nei_inv.insert(it);
                 mm.push({it, cir.GetSum(it)});
                 ++it;
             }
